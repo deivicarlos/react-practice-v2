ea95883f9b698d58a8334a446014406679da2870
Carlos Garcia <deivicarlos@gmail.com>
2026-05-29 18:40:18 -0600

    fixed tsconfig

--- counter/src/components/MainContent.tsx
+++ counter/src/components/MainContent.tsx
@@ -1,4 +1,4 @@
-import useCounter from "hooks/useCounter";
+import useCounter from "@/hooks/useCounter";
 
 type ButtonProps = React.ComponentPropsWithoutRef<"button"> & {
     variant?: "primary" | "danger"
